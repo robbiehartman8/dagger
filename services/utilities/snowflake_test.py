7a712ffc23775183f11dd9c3e4233cd74a300424
Robert Hartman <robbie.hartman8@gmail.com>
2023-02-21 16:37:49 -0500

    create and update cleanup

--- services/utilities/snowflake_test.py
+++ services/utilities/snowflake_test.py
@@ -1,36 +1,18 @@
+
 import re
 
+name_list = ['robert', 'm', 'hartman', 'rob', 'm', 'hartman']
 
-legal_first_name = "robert"
-legal_middle_name = "maurice"
-legal_last_name = "hartman"
+def cleanName(name_list):
+    # change to have special characters later
+    for index in range(len(name_list)):
+        name = name_list[index].strip().lower()
+        name = re.sub(r'[^a-zA-Z0-9]', '', name)
+        name_list[index] = name
 
-preferred_first_name = "rob"
-preferred_middle_name = ""
-preferred_last_name = "hartman"
+    return name_list[0:3], name_list[3:]
 
-def cleanName(first_name, middle_name, last_name):
-    first_name = first_name.strip().lower()
-    first_name = re.sub(r'[^a-zA-Z0-9]', '', first_name)
-    middle_name = middle_name.strip().lower()
-    middle_name = re.sub(r'[^a-zA-Z0-9]', '', middle_name)
-    last_name = last_name.strip().lower()
-    last_name = re.sub(r'[^a-zA-Z0-9]', '', last_name)
+print(cleanName(name_list))
 
-    return [first_name, middle_name, last_name]
 
-def checkNameStatus(legal_name_list, preferred_name_list):
-    name_status = False
-    for i in zip(legal_name_list, preferred_name_list):
-        if i[0] != i[1]:
-            name_status = True
-            break
-    if name_status is True:
-        return name_status, preferred_name_list
-    else:
-        return name_status, legal_name_list
 
-legal_name_list = cleanName(legal_first_name, legal_middle_name, legal_last_name)
-preferred_name_list = cleanName(preferred_first_name, preferred_middle_name, preferred_last_name)
-
-# print(checkNameStatus([legal_first_name, legal_middle_name, legal_last_name], [preferred_first_name, preferred_middle_name, preferred_last_name]))
