1915f95634f7ab297e6496e197f674969d3019a8
Robert Hartman <robbie.hartman8@gmail.com>
2023-02-23 01:16:42 -0500

    commit

---
+++ services/clients/identity_client.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.insert(1, "/Users/roberthartman/Desktop/repos/dagger/services/identity")
+import grpc
+import identity_pb2
+import identity_pb2_grpc
+
+def readIdentity(hr_id_number):
+    request = {"hr_id": hr_id_number}
+    with grpc.insecure_channel('localhost:50052') as channel:
+        stub = identity_pb2_grpc.IdentityStub(channel)
+        response = stub.readIdentity(identity_pb2.readData(**request))
+    print(response)
+
+def createUpdateIdentity(hr_id):
+    request = {"hr_id": hr_id, "legal_first_name": "robert", "legal_middle_name": "maurice", "legal_last_name": "hartman", 
+               "preferred_first_name": "robert", "preferred_middle_name": "hartman", "preferred_last_name": "hartman"
+              }
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = identity_pb2_grpc.IdentityStub(channel)
+        response = stub.createUpdateIdentity(identity_pb2.hrData(**request))
+        print(response)
+
+def appearUserIdClient(identity_id):
+    request = {"identity_id": "3ac4cd09d0d7b58138018bf0d009f7f1", "first_name": "robert", "middle_name": "maurice", "last_name": "hartman"}
+    with grpc.insecure_channel('localhost:50054') as channel:
+        stub = identity_pb2_grpc.IdentityStub(channel)
+        response = stub.appearUserId(identity_pb2.getUserId(**request))
+    print(response)
+
+if __name__ == '__main__':
+    appearUserIdClient("991a286e85fa0c752817d77922671baa")
---
+++ services/identity/delete/server_delete_identity.py
@@ -0,0 +1,72 @@
+# take out with docker
+import sys
+sys.path.insert(1, "/Users/roberthartman/Desktop/repos/dagger/services/utilities")
+sys.path.insert(1, "/Users/roberthartman/Desktop/repos/dagger/services/identity")
+
+# import python libs
+from concurrent import futures
+import grpc
+import identity_pb2
+from identity_pb2 import hrData, hrDataMessage
+import identity_pb2_grpc
+from google.protobuf.json_format import MessageToDict
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s')
+# import custom libs
+from snowflake_connection_utilities import SnowflakeConnetion
+import identity_constants as const
+from service_utilities import ServiceUtilities
+from query_utilities import QueryUtilities
+from identity_utilities import IdentityUtilities
+from call_service_utilities import CallService
+from config_utilities import service_ports, service_workers
+
+class Identity(identity_pb2_grpc.IdentityServicer):
+
+    def __init__(self):
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.INFO)
+
+        self.snowflake_connection = SnowflakeConnetion().getConnection(self.logger)
+        self.request_attributes = list(hrData.DESCRIPTOR.fields_by_name.keys())
+        self.reponse_attributes = list(hrDataMessage.DESCRIPTOR.fields_by_name.keys())
+
+        self.logger.info(f"Server started running on port: {service_ports['createUpdateIdentity']}")
+
+    def createUpdateIdentity(self, request, context):        
+
+        reuqest_data = MessageToDict(request, preserving_proto_field_name=True)
+        request_data = ServiceUtilities().cleanCreateUpdateRequest(reuqest_data, self.request_attributes)
+
+        try:
+            if request_data["hr_id"] != "":
+
+                request_data["identity_id"] = ServiceUtilities().getTablePK("identity", request_data["hr_id"])
+
+                legal_name_list, preferred_name_list = IdentityUtilities().cleanName([request.legal_first_name, request.legal_middle_name, request.legal_last_name, request.preferred_first_name, request.preferred_middle_name, request.preferred_last_name])
+                name_status, name_list = IdentityUtilities().checkNameStatus(legal_name_list, preferred_name_list)
+                request_data["use_preferred_name"] = name_status
+
+                self.logger.info("Getting user_id")
+                get_user_id_request = {"identity_id": request_data["identity_id"], "first_name": name_list[0], "middle_name": name_list[1], "last_name": name_list[2]}
+                request_data["user_id"] = MessageToDict(CallService().callAppearUserId("server_appear_user_id_identity", service_ports["appearUserId"], get_user_id_request), preserving_proto_field_name=True)["user_id"]
+                # request_data["user_id"] = MessageToDict(CallService().callAppearUserId("localhost", service_ports["appearUserId"], get_user_id_request), preserving_proto_field_name=True)["user_id"]
+                self.logger.info("Got user_id")
+
+                merge_statement = QueryUtilities().createMergeStatement(request_data, const.create_update_identity_query)
+                QueryUtilities().executeMerge(merge_statement, self.snowflake_connection, self.logger)
+                        
+                response_data = ServiceUtilities().getCreateUpdateResponse(const.create_update_success_message, self.reponse_attributes, request_data)       
+        except:
+            response_data = ServiceUtilities().getCreateUpdateResponse(const.create_update_fail_message, ["status_message"], request_data)
+
+        response_data = identity_pb2.hrDataMessage(**response_data)
+
+        return response_data
+
+if __name__ == "__main__":
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=service_workers["createUpdateIdentity"]))
+    identity_pb2_grpc.add_IdentityServicer_to_server(Identity(), server)
+    server.add_insecure_port(f"[::]:{service_ports['createUpdateIdentity']}")
+    server.start()
+    server.wait_for_termination()
